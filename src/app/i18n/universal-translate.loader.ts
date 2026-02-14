533033a393bf6b7632b0de9cc64595621816943e
Vadim <a-yva-2013@mail.ru>
2026-02-14 16:02:45 +0400

    ✨ (settings.json, app.routes.server.ts, universal-translate.loader.ts, home-page.html, en.json, ru.json): добавлены новые функции и улучшения локализации ♻️ (universal-translate.loader.ts): удален ненужный код для серверного рендеринга Изменения включают добавление новых ключей для перевода в файлы локализации, улучшение структуры кода и удаление устаревшего кода, связанного с серверным рендерингом. Это делает приложение более чистым и улучшает поддержку многоязычности, обеспечивая более точные переводы и улучшая пользовательский интерфейс.

--- src/app/i18n/universal-translate.loader.ts
+++ src/app/i18n/universal-translate.loader.ts
@@ -1,29 +1,12 @@
-import { inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformServer } from '@angular/common';
+import { inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslationObject } from '@ngx-translate/core';
-import { Observable, from } from 'rxjs';
+import { Observable } from 'rxjs';
 
 export class UniversalTranslateLoader implements TranslateLoader {
-  private platformId = inject(PLATFORM_ID);
   private http = inject(HttpClient);
 
   getTranslation(lang: string): Observable<TranslationObject> {
-    if (isPlatformServer(this.platformId)) {
-      // SSR: читаем файл с диска
-      return from(
-        (async () => {
-          const fs = await import('node:fs/promises');
-          const path = await import('node:path');
-
-          const filePath = path.join(process.cwd(), 'src', 'assets', 'i18n', `${lang}.json`);
-          const content = await fs.readFile(filePath, 'utf-8');
-          return JSON.parse(content) as TranslationObject;
-        })(),
-      );
-    }
-
-    // Browser: грузим из assets
     return this.http.get<TranslationObject>(`assets/i18n/${lang}.json`);
   }
 }
