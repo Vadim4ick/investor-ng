c9a619a98c6dccf02d6ab5582c0d0a7328225125
Vadim <a-yva-2013@mail.ru>
2026-02-26 22:37:38 +0400

    реализация страницы для подсчета доходов/расходов

--- src/app/pages/calculation/calculation.ts
+++ src/app/pages/calculation/calculation.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
 import { AppContainerComponent } from '@/shared/layouts/app-container';
+import {
+  UbPaginationEllipsisComponent,
+  UbPaginationDirective,
+  UbPaginationContentDirective,
+  UbPaginationItemDirective,
+  UbPaginationPreviousDirective,
+  UbPaginationNextDirective,
+  UbPaginationLinkDirective,
+} from '@/shared/ui/pagination';
 
 @Component({
   selector: 'calculation',
-  imports: [AppContainerComponent],
+  imports: [
+    AppContainerComponent,
+    UbPaginationEllipsisComponent,
+    UbPaginationDirective,
+    UbPaginationContentDirective,
+    UbPaginationItemDirective,
+    UbPaginationPreviousDirective,
+    UbPaginationNextDirective,
+    UbPaginationLinkDirective,
+  ],
   templateUrl: './calculation.html',
 })
 export class Calculation {}
